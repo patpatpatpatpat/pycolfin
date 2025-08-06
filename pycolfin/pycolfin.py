bfefd97414b4884c227b4d109290b5f3382fad30
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2025-08-06 14:01:42 +0800

    Prevent regex  warning  # If applied, this commit will...

--- pycolfin/pycolfin.py
+++ pycolfin/pycolfin.py
@@ -16,7 +16,7 @@ def validate_user_id(user_id):
     Raise an exception if the user_id does not follow the correct format.
     """
     invalid_user_id_msg = 'Invalid User ID. Please use a dash (-). Example: 1234-4567'
-    user_id_pattern = re.compile('(\d{4}-\d{4})')
+    user_id_pattern = re.compile('(\\d{4}-\\d{4})')
 
     if len(user_id) > USER_ID_MAX_LENGTH or not user_id_pattern.match(user_id):
         raise Exception(invalid_user_id_msg)
